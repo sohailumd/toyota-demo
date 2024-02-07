019f303529f503989a6a5458480cf1b473810441
Sohail Mohammed <cloudtraining44@gmail.com>
2024-02-06 20:10:52 -0600

    Added sql create

---
+++ psql_pipeline/TableCreate.sql
@@ -0,0 +1,10 @@
+CREATE TABLE demotable (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    Gender CHAR(1),
+    Department VARCHAR(50),
+    Salary DECIMAL(10, 2),
+    HireDate DATE
+    );
